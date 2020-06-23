1ff4ff005723125a71eab25d5e78227a37ceb0bf
JackyYao <921195777@qq.com>
2020-06-23 14:47:47 +0800

    删除了测试文件，上传Unity-TD项目

---
+++ Assets/Scripts/Soldier_Base.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Soldier_Base : MonoBehaviour {
+
+    public float hp;
+    public GameObject target;
+    public float attackRange;
+    public float attackCoolDown;
+
+    private float timer;
+    private NavMeshAgent NMA;
+
+
+    void Start()
+    {
+        NMA = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+
+        if (target == null) return;
+
+        timer += Time.fixedDeltaTime;
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+
+        if (dist > attackRange)
+        {
+            NMA.SetDestination(target.transform.position);
+
+        }
+        else
+        {
+            NMA.ResetPath();
+            if (timer > attackCoolDown)
+            {
+                transform.LookAt(target.transform.position);
+                   //这边加一个攻击动作
+                   timer = 0f;
+            }
+        }
+
+    }
+}
